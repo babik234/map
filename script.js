0905d125dff02902290d1032dabb908bc2031ab3
babik234 <babik234@seznam.cz>
2024-06-25 12:50:55 +0200

    done

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 let map = L.map("map").setView([49.8037633, 15.4749126], 8);
 
+let ortofoto = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
+    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
+});
+
 let osm = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 });
+
 var sellerIcon = L.icon({
     iconUrl: "https://www.iconpacks.net/icons/2/free-store-icon-2017-thumb.png",
     iconSize: [35, 35],
 });
+
 var museumIcon = L.icon({
     iconUrl: "https://upload.wikimedia.org/wikipedia/commons/5/5e/Map_symbol_museum_02.png",
     iconSize: [35, 35],
 });
 
-const museumsArray = [];
-const sellersArray = [];
+let museumsArray = [];
+let sellersArray = [];
+
 osm.addTo(map);
 fetchJSONData();
 
@@ -24,7 +31,7 @@ async function fetchJSONData() {
             throw new Error(`HTTP error! Status: ${res.status}`);
         }
         const data = await res.json();
-        museumsArray.push(...data); // Spread the array elements into museumsArray
+        museumsArray = data;
         console.log(museumsArray);
     } catch (error) {
         console.error("Unable to fetch data:", error);
@@ -36,61 +43,49 @@ async function fetchJSONData() {
             throw new Error(`HTTP error! Status: ${res.status}`);
         }
         const data = await res.json();
-        sellersArray.push(...data); // Spread the array elements into museumsArray
+        sellersArray = data;
         console.log(sellersArray);
     } catch (error) {
         console.error("Unable to fetch data:", error);
     }
-    let markers = L.markerClusterGroup();
+
+    let museumMarkers = L.markerClusterGroup();
+    let sellerMarkers = L.markerClusterGroup();
 
     museumsArray.forEach((e) => {
-        let lng = e.lng;
-        let lat = e.lat;
-        let title = e.title;
-        let address = e.address;
-        let city = e.city;
-        let marker = L.marker([lat, lng], { icon: museumIcon });
-        popupContent = L.popup([lat, lng], {
-            content: `
-          <div  class="title"> <p><h2>Název:</h2> ${title}</p> </br></div>
-             <div  class="title"> <p><h2>Adresa:</h2> ${address}</p> </br> </div>
-               <div  class="title"> <p><h2>Město:</h2> ${city}</p> </br> </div>
-         
-            `,
-        });
-        marker.addTo(map);
-        marker.bindPopup(popupContent).openPopup();
-        //   markers.addLayer(marker);
-        // map.addLayer(markers);
+        let marker = L.marker([e.lat, e.lng], { icon: museumIcon });
+        let popupContent = `
+            <div class="title"><h2>Název:</h2> ${e.title}</div>
+            <div class="title"><h2>Adresa:</h2> ${e.address}</div>
+            <div class="title"><h2>Město:</h2> ${e.city}</div>
+        `;
+        marker.bindPopup(popupContent);
+        museumMarkers.addLayer(marker);
     });
 
     sellersArray.forEach((e) => {
-        let lng = e.lng;
-        let lat = e.lat;
-        let title = e.title;
-        let address = e.address;
-        let city = e.city;
-        let marker = L.marker([lat, lng], { icon: sellerIcon });
-        popupContent = L.popup([lat, lng], {
-            content: `
-            <div  class="title"> <p><h2>Název:</h2> ${title}</p> </br></div>
-             <div  class="title"> <p><h2>Adresa:</h2> ${address}</p> </br> </div>
-               <div  class="title"> <p><h2>Město:</h2> ${city}</p> </br> </div>
-         
-      
-            `,
-        });
-        marker.addTo(map);
-        marker.bindPopup(popupContent).openPopup();
-        // markers.addLayer(marker);
-        //map.addLayer(markers);
+        let marker = L.marker([e.lat, e.lng], { icon: sellerIcon });
+        let popupContent = `
+            <div class="title"><h2>Název:</h2> ${e.title}</div>
+            <div class="title"><h2>Adresa:</h2> ${e.address}</div>
+            <div class="title"><h2>Město:</h2> ${e.city}</div>
+        `;
+        marker.bindPopup(popupContent);
+        sellerMarkers.addLayer(marker);
     });
+
+    map.addLayer(museumMarkers);
+    map.addLayer(sellerMarkers);
+
+    let baseLayers = {
+        OpenStreetMap: osm,
+        Ortofoto: ortofoto,
+    };
+
+    let overLayers = {
+        Museums: museumMarkers,
+        Sellers: sellerMarkers,
+    };
+
+    L.control.layers(baseLayers, overLayers).addTo(map);
 }
-
-/*
-let ortofoto = L.tileLayer("https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}", {
-    attribution: "Tiles &copy; Esri &mdash; Source: Esri, i-cubed, USDA, USGS, AEX, GeoEye, Getmapping, Aerogrid, IGN, IGP, UPR-EGP, and the GIS User Community",
-});
-
-ortofoto.addTo(map);
-*/
